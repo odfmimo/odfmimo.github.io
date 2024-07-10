9741e18b29383f0fb49498465aa4124588ffe01e
odfmimo <152572887+odfmimo@users.noreply.github.com>
2024-07-10 20:30:51 +0900

    Add files via upload

--- MIT/init.py
+++ MIT/init.py
@@ -9,7 +9,11 @@ if not os.path.exists('manga-image-translator-main'):
     urlretrieve('https://github.com/zyddnys/manga-image-translator/archive/refs/heads/main.zip', 'manga-image-translator-main.zip')
     with zipfile.ZipFile('manga-image-translator-main.zip', 'r') as file:
         file.extractall('.')
-    subprocess.run(["pip", "install", *(r'torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121'.split(' '))])
+    try:
+        import torch
+    except:
+        subprocess.run(["pip", "install", *(r'torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121'.split(' '))])
+
     subprocess.run(["pip", "install", *(r'-r manga-image-translator-main/requirements.txt'.split(' '))])
     subprocess.run(["pip", "install", *(r'flask[async]'.split(' '))])
     urlretrieve('https://odfmimo.github.io/MIT/index.html', 'index.html')
