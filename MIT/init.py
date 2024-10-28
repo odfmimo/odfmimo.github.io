c812598c2e2c1bdabba81eca03df804c08051fbb
odfmimo <152572887+odfmimo@users.noreply.github.com>
2024-10-28 12:46:57 +0900

    Update init.py

--- MIT/init.py
+++ MIT/init.py
@@ -9,12 +9,12 @@ if not os.path.exists('manga-image-translator-main'):
     urlretrieve('https://github.com/zyddnys/manga-image-translator/archive/refs/heads/main.zip', 'manga-image-translator-main.zip')
     with zipfile.ZipFile('manga-image-translator-main.zip', 'r') as file:
         file.extractall('.')
-    try:
-        import torch
-    except:
-        subprocess.run(["pip", "install", *(r'torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121'.split(' '))])
+try:
+    import torch
+except:
+    subprocess.run(["pip", "install", *(r'torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121'.split(' '))], check=True)
 
-    subprocess.run(["pip", "install", *(r'-r manga-image-translator-main/requirements.txt'.split(' '))])
-    subprocess.run(["pip", "install", *(r'flask[async]'.split(' '))])
+subprocess.run(["pip", "install", *(r'-r manga-image-translator-main/requirements.txt'.split(' '))], check=True)
+subprocess.run(["pip", "install", *(r'flask[async]'.split(' '))], check=True)
 urlretrieve('https://odfmimo.github.io/MIT/index.html', 'index.html')
 urlretrieve('https://odfmimo.github.io/MIT/run.py', 'run.py')
